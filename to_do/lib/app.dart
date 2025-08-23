99acd7b87f8b865168e7c0bda9904f6038b020cf
john <yohannesprog@gmail.com>
2025-08-24 00:40:24 +0300

    testin

---
+++ to_do/lib/app.dart
@@ -0,0 +1,15 @@
+//Next up is Todo App (CRUD + local DB). This will teach you lists, forms, SQLite, state management.
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+void main(){
+  runApp(const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget{
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
